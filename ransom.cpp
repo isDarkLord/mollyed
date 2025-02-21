6e3271fb861ec7d4bb24c5d87d1acb346e3aa8ea
isDarkLord <darklorddarkside653@gmail.com>
2025-02-21 20:01:12 +0600

    fdf

---
+++ ransom.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <winbase.h>
+
+int main(){
+
+   /*Working on adding something to show */
+
+   CopyFile(
+    "C://Users",
+    "C://$Recycle.Bin",
+    false
+);
+   
+}
